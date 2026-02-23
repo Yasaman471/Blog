e58dbac598743b8e3550c120927c9e3b6e791d89
Yasaman Abedi <Yasaman471@gmail.com>
2026-02-23 12:11:35 +0330

    feat: setup Apollo Client with Hygraph and install Material UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+
+const query = gql`
+  query {
+    authors {
+      name
+    }
+  }
+`;
+
 function App() {
+  const { loading, error, data } = useQuery(query);
+  console.log({ loading, error, data });
+
   return (
     <div>
       <h1>Blog</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+
 import "./global.css";
 import App from "./App.jsx";
 
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://us-west-2.cdn.hygraph.com/content/cma5bvoyi016q06w8ze6zpduv/master",
+  }),
+  cache: new InMemoryCache(),
+});
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
 );
